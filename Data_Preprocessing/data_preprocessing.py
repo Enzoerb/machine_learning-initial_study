7c78f1082641a2d76ae0f07aa4b14c73ab9f34cb
Enzo Erbert <enzoerbert@gmail.com>
2021-01-11 14:53:55 -0300

    spliting sets and feature scaling

--- Data_Preprocessing/data_preprocessing.py
+++ Data_Preprocessing/data_preprocessing.py
@@ -1,15 +1,14 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-
 
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, 1:])
@@ -23,3 +22,9 @@ X = np.array(column_transformer.fit_transform(X))
 
 label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+standard_scaler = StandardScaler()
+X_train[:, 3:] = standard_scaler.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = standard_scaler.transform(X_test[:, 3:])
